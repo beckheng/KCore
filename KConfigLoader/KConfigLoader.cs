95bd041f9021c8fa07b4473bd7ac92f317f067f6
Beckheng Lam <beckheng@gmail.com>
2017-11-23 01:34:28 +0800

    添加配置表加载工具

---
+++ KConfigLoader/KConfigLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using KData;
+
+namespace KCore
+{
+
+	[DisallowMultipleComponent]
+	public class KConfigLoader : MonoBehaviour
+	{
+
+		public static bool allConfigLoaded = false;
+
+		// Use this for initialization
+		void Start()
+		{
+			// 用协程来加载配置表
+			StartCoroutine(LoadAllConfigData());
+		}
+		
+		private IEnumerator LoadAllConfigData()
+		{
+			allConfigLoaded = false;
+
+			yield return StartCoroutine(ConfigLoaderAutoGen.LoadAllData());
+			
+			allConfigLoaded = true;
+		}
+
+	}
+
+}
