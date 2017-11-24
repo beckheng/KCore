d7f17d8f05f4433c2e304ccb0f7444f9fe4cb632
Beckheng Lam <beckheng@gmail.com>
2017-11-24 23:03:56 +0800

    添加菜单: 根据配置表生成.json文件及.cs代码
    ProcessUtilEditor: 用于调用外部命令

---
+++ Editor/ConfigEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+using System.IO;
+
+using YamlDotNet.RepresentationModel;
+
+namespace KCoreEditor
+{
+	public class ConfigEditor : EditorWindow
+	{
+
+		/// <summary>
+		/// 根据配置表生成.json文件及.cs代码
+		/// </summary>
+		[MenuItem("KCore/生成配置数据及.cs代码", false, 1051)]
+		private static void GenExcelAndCS()
+		{
+			string yamlConfigFile = Application.dataPath + "/../../config.yml";
+			
+			string yamlConfigContent = File.ReadAllText(yamlConfigFile);
+			
+			// Load the stream
+			var yaml = new YamlStream();
+			yaml.Load(new StringReader(yamlConfigContent));
+
+			// Examine the stream
+			var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+
+			string creatorPath = mapping.Children[new YamlScalarNode("CreatorPath")].ToString();
+			
+			string projectPath = Application.dataPath + "/../..";
+
+			ProcessUtilEditor.StartProcess("perl", creatorPath + "/gen_protobuf_excel.pl " + projectPath);
+			ProcessUtilEditor.StartProcess("perl", creatorPath + "/gen_protobuf_cs.pl " + projectPath);
+		}
+
+	}
+
+}
---
+++ Editor/ProcessUtilEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace KCoreEditor
+{
+
+	/// <summary>
+	/// 仅在编辑器模式下调用
+	/// </summary>
+	public class ProcessUtilEditor : EditorWindow
+	{
+
+		/// <summary>
+		/// 调用外部进程
+		/// </summary>
+		public static void StartProcess(string processPath, string args)
+		{
+			processPath = processPath.Replace("/", "\\");
+
+			System.Diagnostics.Process p = new System.Diagnostics.Process();
+			p.StartInfo.FileName = processPath;
+
+			p.StartInfo.Arguments = args;
+
+			p.StartInfo.UseShellExecute = true;
+			p.StartInfo.RedirectStandardInput = false;
+			p.StartInfo.RedirectStandardOutput = false;
+			p.StartInfo.RedirectStandardError = false;
+
+			p.Start();
+			p.WaitForExit();
+		}
+
+		public static void OpenURL(string url)
+		{
+			System.Diagnostics.Process.Start(url);
+		}
+
+	}
+
+}
