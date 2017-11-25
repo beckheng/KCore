f334c37840d89646148955af0dbb0c5c13c4f8be
Beckheng Lam <beckheng@gmail.com>
2017-11-25 19:14:53 +0800

    添加编辑器菜单: KCore/生成配置数据(无结构变化)

--- Editor/ConfigEditor.cs
+++ Editor/ConfigEditor.cs
@@ -12,15 +12,45 @@ namespace KCoreEditor
 	{
 
 		/// <summary>
+		/// 根据配置表生成.json文件
+		/// </summary>
+		[MenuItem("KCore/生成配置数据(无结构变化)", false, 1051)]
+		private static void GenExcelOnly()
+		{
+			YamlMappingNode mapping = GetYamlMappingNode();
+
+			string creatorPath = mapping.Children[new YamlScalarNode("CreatorPath")].ToString();
+
+			string projectPath = Application.dataPath + "/../..";
+
+			ProcessUtilEditor.StartProcess("perl", creatorPath + "/gen_protobuf_excel.pl " + projectPath);
+		}
+
+		/// <summary>
 		/// 根据配置表生成.json文件及.cs代码
 		/// </summary>
-		[MenuItem("KCore/生成配置数据及.cs代码", false, 1051)]
+		[MenuItem("KCore/生成配置数据及.cs代码", false, 1052)]
 		private static void GenExcelAndCS()
 		{
+			YamlMappingNode mapping = GetYamlMappingNode();
+			
+			string creatorPath = mapping.Children[new YamlScalarNode("CreatorPath")].ToString();
+			
+			string projectPath = Application.dataPath + "/../..";
+
+			ProcessUtilEditor.StartProcess("perl", creatorPath + "/gen_protobuf_excel.pl " + projectPath);
+			ProcessUtilEditor.StartProcess("perl", creatorPath + "/gen_protobuf_cs.pl " + projectPath);
+		}
+
+		/// <summary>
+		/// 加载由GameCreator生成的YAML配置文件
+		/// </summary>
+		private static YamlMappingNode GetYamlMappingNode()
+		{
 			string yamlConfigFile = Application.dataPath + "/../../config.yml";
-			
+
 			string yamlConfigContent = File.ReadAllText(yamlConfigFile);
-			
+
 			// Load the stream
 			var yaml = new YamlStream();
 			yaml.Load(new StringReader(yamlConfigContent));
@@ -28,12 +58,7 @@ namespace KCoreEditor
 			// Examine the stream
 			var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
 
-			string creatorPath = mapping.Children[new YamlScalarNode("CreatorPath")].ToString();
-			
-			string projectPath = Application.dataPath + "/../..";
-
-			ProcessUtilEditor.StartProcess("perl", creatorPath + "/gen_protobuf_excel.pl " + projectPath);
-			ProcessUtilEditor.StartProcess("perl", creatorPath + "/gen_protobuf_cs.pl " + projectPath);
+			return mapping;
 		}
 
 	}
