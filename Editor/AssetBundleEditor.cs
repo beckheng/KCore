62509a3e4eefe54742559b6bfb474430b6d14821
Beckheng Lam <beckheng@gmail.com>
2017-11-28 22:49:35 +0800

    将UI的预制打为AB

--- Editor/AssetBundleEditor.cs
+++ Editor/AssetBundleEditor.cs
@@ -9,7 +9,7 @@ namespace KCoreEditor
 
 	public class AssetBundleEditor : Editor
 	{
-
+		
 		/// <summary>
 		/// 加载所选的AB资源
 		/// </summary>
@@ -38,6 +38,49 @@ namespace KCoreEditor
 		private static void BuildSceneAB()
 		{
 			BuildPipeline.BuildAssetBundles("Assets/OOO");
+		}
+
+		[MenuItem("KCore/Assetbundle/Buil AB(Selected OR All)", false, 1071)]
+		public static void BuildAllAB()
+		{
+			var obj = Selection.activeObject;
+			if (obj == null)
+			{
+				Debug.LogError("Nothing|selected");
+				return;
+			}
+			
+			string theAssetPath = AssetDatabase.GetAssetPath(obj);
+			if (!theAssetPath.StartsWith("Assets/Prefabs/"))
+			{
+				Debug.LogError("none|a|Prefabs|Under|Assets/Prefabs");
+				return;
+			}
+
+			string outputPath = string.Empty;
+
+			
+			if (theAssetPath.Contains("/View/"))
+			{
+				outputPath = "Assets/StreamingAssets/View";
+			}
+
+			if (string.IsNullOrEmpty(outputPath))
+			{
+				Debug.LogError("no|detected|outputPath|for|" + theAssetPath);
+				return;
+			}
+
+			AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+
+			buildMap[0].assetBundleName = obj.name + ".u3d";
+			buildMap[0].assetNames = new string[] { AssetDatabase.GetAssetPath(obj) };
+			
+			BuildPipeline.BuildAssetBundles(outputPath, buildMap, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+
+			Debug.Log("BuildAB|OK|outputPath|" + outputPath + "|name|" + obj.name + ".u3d");
+
+			AssetDatabase.Refresh();
 		}
 
 	}
