72fc510a32ffe06211f5095eee970c39a34a6221
Beckheng Lam <beckheng@gmail.com>
2017-11-30 21:45:25 +0800

    KAssetPostprocessor 处理 资源导入后设置各个推荐的参数

---
+++ Editor/KAssetPostprocessor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class KAssetPostprocessor : AssetPostprocessor
+{
+
+	/// <summary>
+	/// 设置音频文件导入后的推荐参数
+	/// </summary>
+	/// <param name="ac"></param>
+	private void OnPostprocessAudio(AudioClip audioClip)
+	{
+		if (assetPath.StartsWith("Assets/Arts/Sounds/"))
+		{
+			AudioImporter audioImporter = assetImporter as AudioImporter;
+
+			AudioImporterSampleSettings sampleSettings = audioImporter.defaultSampleSettings;
+			sampleSettings.loadType = AudioClipLoadType.Streaming;
+			
+			audioImporter.defaultSampleSettings = sampleSettings;
+		}
+	}
+
+	/// <summary>
+	/// 设置模型导入后的推荐参数,并且有编写的规则, XXX@action这样是表示有动作的
+	/// </summary>
+	private void OnPostprocessModel(GameObject go)
+	{
+		if (assetPath.StartsWith("Assets/Arts/Models/"))
+		{
+			ModelImporter modelImporter = assetImporter as ModelImporter;
+
+			if (assetPath.Contains("@"))
+			{
+				//包含动作的文件
+				modelImporter.animationType = ModelImporterAnimationType.Generic;
+
+				modelImporter.sourceAvatar = null;
+			}
+			else
+			{
+				//模型本身
+				modelImporter.animationType = ModelImporterAnimationType.Generic;
+				modelImporter.importAnimation = false;
+
+				modelImporter.sourceAvatar = null;
+			}
+		}
+	}
+
+}
