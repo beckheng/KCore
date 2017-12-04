83d7f971a0b08121f9e3536ac1c2eb5bb17d946f
Beckheng Lam <beckheng@gmail.com>
2017-12-05 01:03:22 +0800

    用于运行时动态切换动作

---
+++ Editor/KAnimatorEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Animator))]
+public class KAnimatorEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		if (Application.isPlaying)
+		{
+			Animator ani = target as Animator;
+			if (ani.isInitialized)
+			{
+				foreach (var item in ani.runtimeAnimatorController.animationClips)
+				{
+					if (GUILayout.Button("播放|动作|" + item.name))
+					{
+						ani.CrossFade(item.name, 0f);
+					}
+				}
+			}
+		}
+	}
+
+}
