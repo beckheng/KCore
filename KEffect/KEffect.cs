8ca0d073971ddd026dc15ae7006b2fd198f60ced
Beckheng Lam <beckheng@gmail.com>
2017-12-05 22:16:37 +0800

    添加KEffect,用于管理特效加载(与销毁???哈哈)

---
+++ KEffect/KEffect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using KCore;
+
+public class KEffect {
+
+	/// <summary>
+	/// 在预加载的AB实例化特效,可以指定parent,为方便额外的控制,返回值是Transform
+	/// </summary>
+	public static Transform PlayFX(string abName, Transform parent = null)
+	{
+		Transform eff = KAssetBundle.InstantiateEffect(abName);
+		if (parent != null)
+		{
+			eff.SetParent(parent, false);
+		}
+
+		return eff;
+	}
+	
+}
