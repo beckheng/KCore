de7654b73aebc3e57b4f4005b97a1095e6f13893
Beckheng Lam <beckheng@gmail.com>
2017-12-08 20:20:56 +0800

    添加 KBehaviour

---
+++ KBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace KCore
+{
+
+	/// <summary>
+	/// 继承于MonoBehaviour,一些继承于MonoBehaviour的Component优先使用这个类,会有一些最佳实践封装一下
+	/// </summary>
+	public abstract class KBehaviour : MonoBehaviour
+	{
+
+		protected Transform tran = null;
+		
+		protected void Awake()
+		{
+			tran = this.transform;
+			OnAwake();
+		}
+
+		/// <summary>
+		/// 用于继承类Awake()时调用
+		/// </summary>
+		protected abstract void OnAwake();
+	}
+
+}
